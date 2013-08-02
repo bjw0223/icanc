fd0f284cf7f7a74450e5161b31f6f30214817995
root <root@YM-Ubuntu-PC.(none)>
2013-08-02 19:43:46 +0900

    ym

--- application/views/board/show.php
+++ application/views/board/show.php
@@ -11,11 +11,14 @@
 	{
 		echo 	"<tr><td>$data->srl</td><td><a href='".base_url()."index.php/board/show/$data->srl'>$data->title</a></td><td>$data->writer</td><td>$data->modified_date</td><td>$data->count</td></tr>";
 		
-	} 
+	 
 ?>		
 	</tbody>
 </table>
-
+<?php
+	echo $data->text;
+}
+?>
 
 </div>
 
