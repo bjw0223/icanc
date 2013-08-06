f9aae9adf7c5f374b211222c18152b7bb54a0786
bang jiwoong <bjw0223@naver.com>
2013-08-06 20:14:14 +0900

    jiwoong

--- application/views/board/show.php
+++ application/views/board/show.php
@@ -1,34 +1,40 @@
-<div id="faq" class="col-lg-9" style="margin-top:30px;">
+<style>
 
-	<table class="table table-striped" >
-		<tbody>
-	
+</style>
+<div id="faq" class="col-lg-9" style="margin-top:30px;background-color:#eeeeee;">
+
 <?php
 	$data=$list[0];
-	echo "<div class='row'><legend> 
-			<div class='col-lg-8'><h3>$data->title<h3></div> 
-			<div class='col-lg-4'><h4>임시날짜<h4></div> 
-		</legend></div>
-		<div class='row'><legend>
-			<div class='col-lg-8'>$data->writer</div>
-			<div class='col-lg-4'>
-				조회수 <small>$data->hits</small>  
-				추천수 <small>$data->goods</small> 
-			</div>
-		</legend></div>
-		<div class='row'><legend>
-			<div class='col-lg-12'><br>$data->text<br></div>
-		</legend></div>
-		<div class'row'><legend>
-			<div class='col-lg-2'>좋아요 $data->goods </div>
-			<div class='col-lg-8'></div>
-			<div class='col-lg-2'>목록버튼</div>
-		</legend></div>";	
 ?>		
-		</tbody>
-	</table>
-
+        <div class="row" style="padding:5px;">
+            <div class="col-lg-12" style="min-height:50px;background-color:white;">
+                <p class="col-lg-10" style="font-size:28px;font-weight:bold;"><?=$data->title?></p>
+                <p class="col-lg-2 pull-right"><?//=$data->modified_time?>임시</p>
+            </div>
+        </div>
+        <div class="row" style="padding:5px;">
+            <div class="col-lg-12" style="min-height:35px;background-color:white;">
+                <p class="col-lg-8">작성자 : <?=$data->writer?></p>
+                <p class="col-lg-2">조회수 : <?=$data->hits?></p>
+                <p class="col-lg-2">추천수 : <?=$data->goods?></p>
+            </div>
+        </div>
+        <div class="row" style="padding:5px;">
+            <div class="col-lg-12" style="overflow:auto;min-height:350px;background-color:white;">
+                <?=$data->text?>
+            </div> 
+        </div>
+        <div class="row" style="padding-top:5px;padding-bottom:5px;">
+            <div class="col-lg-11">
+                <button type="button" class="btn btn-info">좋아요 <?=$data->goods?></button>
+            </div>
+            <div class="col-lg-1">
+                <button type="button" class="btn"><i class="icon-reorder icon-large"></i></button>
+            </div>
+        </div>
+        
 </div>
+
 
 <!-- -->
 </div>
