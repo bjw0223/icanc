cf18c362711e93ffb27bdd800a33d2ac03e055fe
Yeomyeong <wymstar@naver.com>
2013-08-12 12:22:52 +0900

    wym

--- user/1/temp/test.c
+++ user/1/temp/test.c
@@ -1,10 +1,35 @@
-#include<stdio.h>
+<div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
 
-// 메인함수
+<h4>A PHP Error was encountered</h4>
+
+<p>Severity: Notice</p>
+<p>Message:  Undefined variable: head</p>
+<p>Filename: quiz/codingQuiz.php</p>
+<p>Line Number: 110</p>
+
+</div>아럼어라밍ㄹ
+  ㅇㄹㅁㅇㄴㄹ
+  ㅁㄴㅇㄹ
+  ㅁㅇㄴㄹ
+  ㅁㄴㅇ
+  	ㄴㅇㄹㄴㅇㄹ
+	ㅇㄹㄴㅇㄹt
+  
+  #include<stdio.h>
+  
 int main()
 {
-    int a;
-    a = 20;    printf("a=%d",a);  // 이 함수의 결과값은?
+  int fuck(void *dd)
+  {
     
-    return 0;
-}
+  }
+}<div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
+
+<h4>A PHP Error was encountered</h4>
+
+<p>Severity: Notice</p>
+<p>Message:  Undefined variable: tail</p>
+<p>Filename: quiz/codingQuiz.php</p>
+<p>Line Number: 118</p>
+
+</div>
