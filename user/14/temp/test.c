5c00e03c4ccbd468bc4d3251b8438afbc84d586b
bang jiwoong <bjw0223@naver.com>
2013-08-13 15:49:08 +0900

    jiwoong

---
+++ user/14/temp/test.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+// 메인함수
+int main()
+{
+    int a;
+    a = 20;int i;
+
+
+  int b = 2;
+  
+  b = b *2;
+  if ( b > 200) return 0;
+  printf("i = %d",i);
+	main();
+     printf("a=%d",a);  // 이 함수의 결과값은?
+   
+    return 0;
+}
