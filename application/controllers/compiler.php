3c476be14748c1522cafda14b871a1892458daf7
miss0110 <eugene@ubuntu>
2013-08-13 14:25:17 +0900

    eugene

--- application/controllers/compiler.php
+++ application/controllers/compiler.php
@@ -13,20 +13,16 @@ class Compiler extends CI_Controller {
     // c파일 생성
     function _createFile($code)
     {
-        // 권한 해제
-        umask(0);
+        umask(0);  //권한 해제
+        $filePath = $this->_filePath();
         
-        // session값에 저장된 mail값 불러오기
-        $this->load->model('user_model');
-        $mail = $this->session->userdata('user_email');
-        $user = $this->user_model->getByEmail(array('email'=>$mail));
-        delete_files('./user/'.$user->id.'/temp/');
+        delete_files($filePath);
 
-        $fp = fopen('/var/www/icanc/user/'.$user->id.'/temp/test.c','w');
+        $fp = fopen($filePath.'test.c','w');
         fwrite($fp,$code);
         fclose($fp);
 
-        return $user;
+        return $filePath;
     }
     
     function compile()
@@ -34,17 +30,15 @@ class Compiler extends CI_Controller {
         $head = $_POST['head'];
         $code = $_POST['code'];
         $tail = $_POST['tail'];
-        $finalCode = $head.$code."\r".$tail;
-       
+
         // textarea에 text값 가져와 \n처리
-        $finalCode = str_replace("\r\n","\n", $finalCode);
-        $finalCode = str_replace("\r","\n", $finalCode);
+        $finalCode = $head."\r".$code."\r".$tail;
         
-        $user = $this->_createFile($finalCode);
+        $filePath = $this->_createFile($finalCode);
             
         // 저장된 code GCC
-        $gcc = 'gcc -o ./user/'.$user->id.'/temp/test ./user/'.$user->id.'/temp/test.c 2> ./user/'.$user->id.'/temp/errmsg.txt';
-        $run = './user/'.$user->id.'/temp/test';
+        $gcc = 'gcc -o '.$filePath.'test '.$filePath.'test.c 2> '.$filePath.'errmsg.txt';
+        $run = $filePath.'test';
         
         // compile
         exec($gcc, $gccOutput, $gccStatus);
@@ -53,9 +47,9 @@ class Compiler extends CI_Controller {
         // 컴파일시 에러 발견됐을때 에러출력
          if($gccOutput == $runOutput)
          {
-             $error = array(read_file('./user/'.$user->id.'/temp/errmsg.txt'));
+             $error = array( read_file($filePath.'errmsg.txt') );
              $error = str_replace("\n","<br>", $error);
-             $error = str_replace('./user/'.$user->id.'/temp/test.c:',">> ", $error);
+             $error = str_replace($filePath.'test.c:',">> ", $error);
              echo json_encode($error);
          }
         // 컴파일 오류가 없을시 실행결과 출력
@@ -64,5 +58,17 @@ class Compiler extends CI_Controller {
              echo json_encode($runOutput);
          }
     }
+    
+    // user/id/temp까지의 파일경로 추출
+    function _filePath()
+    {
+        // session값에 저장된 mail값 불러오기
+        $this->load->model('user_model');
+        $user = $this->user_model->getByEmail(array('email'=>$this->session->userdata('user_email')));
+
+        $SCRIPT_FILENAME = str_replace("index.php","user/",$_SERVER["SCRIPT_FILENAME"]).$user->id.'/temp/';
+        return $SCRIPT_FILENAME;
+    }
+
 
 }
