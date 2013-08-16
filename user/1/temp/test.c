f6a8ade44f5f3631d7d56ab49d82676a9f061bd8
miss0110 <eugene@ubuntu>
2013-08-16 13:37:45 +0900

    eugene

--- user/1/temp/test.c
+++ user/1/temp/test.c
@@ -1,10 +1,7 @@
 #include<stdio.h>
 
-// 메인함수
 int main()
 {
-    int a;
-    a = 20;	a = 10;    printf("a=%d",a);  // 이 함수의 결과값은?
-   
-    return 0;
+    printf("Welcome to I CAN C");
+        return 0;
 }
