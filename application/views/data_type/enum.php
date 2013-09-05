aac0df2088e54cf9e10691348a8b9ab40d0a1b8c
charachara <sjmwoalsl@naver.com>
2013-09-05 21:29:29 +0900

    sjm

--- application/views/data_type/enum.php
+++ application/views/data_type/enum.php
@@ -1,1 +1,14 @@
-enumeration
+<div class="col-lg-9 tutorial_desc">
+    <span class="general">    
+       <ul>
+	       <li>선 언</li>
+		     <li id="list" class="general_sub">[enum] [변수명] { 멤버1, 멤버2, ... };</li>
+             <li id="list" class="general_sub">변수명에 들어갈 수 있는 값을 중괄호 안의 데이터들로 제한하고 있다.</li><br>
+           <li>각 멤버들은 정수형 상수값을 가지고 있으며 첫 멤버가 0이며 다음 멤버부터 1씩 증가한다.</li><br>
+           <li>선언 시 멤버에 임의로 정수 값을 정해줄 수도 있으며 그 다음 멤버부터는 정해준 값에서 1씩 증가한 값을 갖는다.</li>
+             <li id="list" class="general_sub">ex) enum day { sun = 1, mon, tue };</li>
+             <li id="list" class="general_sub" style="text-indent:36px">mon은 2의 값을, tue는 3을 갖는다.</li>
+       </ul>
+    </span>    
+</div>
+
