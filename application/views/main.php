3083eb4afe6e0f7cc5b700de65a50a52a130f93b
bang jiwoong <bjw0223@naver.com>
2013-09-09 14:18:46 +0900

    jiwoong

--- application/views/main.php
+++ application/views/main.php
@@ -10,68 +10,49 @@ body{
 }
 .icanc-main {
     color:white;
-    height:100%
 }
-.carousel-inner > .item > img {
-    width:100%;
+.main-title {
+    font-size:70px;
+    font-weight:bold;
+    letter-spacing:10px;
 }
-.item { 
-    text-align:center;
-    padding-top:10px;
-    padding-bottom:10px;
+.main-btn { 
+    border-radius:10px;
+    border:2px solid white;
+    color:white;
+    cursor:pointer;
+    font-weight:bold;
+    padding:15px;
 }
-.carousel-inner{
-    background-color: rgba(0, 0, 0, 0.1);
+.main-btn:hover { 
+    background-color:white;
+    color:#36545F;
+    cursor:pointer;
+    font-weight:bold;
+    padding:15px;
 }
 </style>
 <script>
-$(document).ready(function() {
-var windowHeight = $(window).height();
-$('.carousel-inner > .item > img').height( windowHeight/100 * 70 ); 
-$('.carousel').carousel({
-      interval: 2000
-      })
-});
+
 </script>
 <div id="main" class="">
     <div class="row icanc-main">
-        <div class="col-lg-5">
-            <p style="font-size:20px;"> reference </p>
-            <p style="font-size:20px;"> tutorial </p>
-            <p style="font-size:20px;"> quiz </p>
-            <p style="font-size:20px;"> freecoding </p>
+        <div class="main-title col-lg-7">
+           ICANC is MOOC 
         </div>
-
-        <div id="carousel-example-generic" class="carousel slide col-lg-7">
-      <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-            </ol>
-
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner">
-                <div class="item active">
-                    <img class="main-img" src="<?=base_url();?>asset/lib/img/main/practice_quiz_result.JPG" alt="...">
-                    <div class="carousel-caption">
-                    Quiz 
-                    </div>
-                </div>
-                <div class="item">
-                    <img class="main-img" src="<?=base_url();?>asset/lib/img/main/freeCoding.JPG" alt="...">
-                    <div class="carousel-caption">
-                    FreeCoding
-                    </div>
-                </div>
-            </div>
-
-            <!-- Controls -->
-            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                <span class="icon-prev"></span>
-            </a>
-            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                <span class="icon-next"></span>
-            </a>
+        <div class="col-lg-5">
+            <p><h1><strong>M</strong><small>Massive</small></h1></p>
+            <p><h1><strong>O</strong><small>Open</small></h1></p>
+            <p><h1><strong>O</strong><small>Online</small></h1></p>
+            <p><h1><strong>C</strong><small>Course</small></h1></p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="main-btn main-login col-lg-6">
+            Login    
+        </div>
+        <div class="main-btn main-Join col-lg-6">
+            Join
         </div>
     </div>
 </div>
