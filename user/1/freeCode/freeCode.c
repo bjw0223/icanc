9ea622d407fb4d411ba6d51b5ba944c66c7dffa7
icanc <bjw0223@naver.com>
2013-09-11 13:10:19 +0900

    server

--- user/1/freeCode/freeCode.c
+++ user/1/freeCode/freeCode.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-void *runCode34567(void *id34567){printf("dd");}
+void *runCode34567(void *id34567){}
 
 void *checkTime34567(void *id34567)
 {
