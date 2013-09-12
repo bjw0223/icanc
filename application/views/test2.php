8d3ca19c6df0dd75ae8e33d0cdb98ea8ff805240
icanc <bjw0223@naver.com>
2013-09-12 15:21:35 +0900

    icanc-server

--- application/views/test2.php
+++ application/views/test2.php
@@ -4,8 +4,20 @@
 <style>
 
 textarea {
-	
-
+	resize:none;	
+	background-color:#eeeeee;
+}
+.text-title {
+	border-top:1px solid black;
+	border-left:1px solid black;
+	border-right:1px solid black;
+	border-bottom:1px solid white;
+	padding:5px 20px 5px 20px;
+	background-color:white;
+}
+.text-file {
+	border:1px solid black;
+	padding:10px;
 }
 
 </style>
@@ -14,10 +26,25 @@ textarea {
 
 <div class="row">
 	<div class="col-lg-6">
-		<textarea class="form-control"></textarea>
+		<div class="text-title-div">
+			<span class="text-title"> A.txt </span>
+		</div>
+		<div class="text-file">
+		asdfas;lkfjasdl;kfja;lkj
+		</div>
 	</div>
 	<div class="col-lg-6">
-		<textarea class="form-control"></textarea>
+		<div class="text-title-div">
+			<span class="text-title"> B.txt </span>
+		</div>
+		<div class="text-file">
+		asdfas;lkfjasdl;kfja;lkj
+		</div>
 	</div>
 
+</div>
+<div class="row">
+	<div class="col-lg-12">
+		fwrite<input type="text">
+	</div>
 </div>
