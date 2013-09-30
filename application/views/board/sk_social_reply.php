c4aebb5f8eef84fb157983faa6fe37b23a62be75
Yeomyeong <wymstar@naver.com>
2013-09-30 16:56:39 +0900

    wym

---
+++ application/views/board/sk_social_reply.php
@@ -0,0 +1,18 @@
+<div id="comments_plugin1"></div>
+<script src="http://comments.skplanetx.com/script/plugin.js"></script>
+<script type="text/javascript">
+	SKP.commentsPlugin({
+		    target_id: 'comments_plugin1',
+			op_app_key: '682abf17-0480-38ed-a76e-5a35849f19e8',
+			// (Optional) 페이지 식별자입니다. 같은 page_id는 같은 댓글들을 표시하며 생략시 페이지URL이 들어갑니다.
+			page_id: 'page001',
+			// (Optional) 아포가토에 표시될 페이지 제목입니다. 생략 시 브라우저 타이틀이 들어갑니다.
+			page_title: 'SKPOP Open Platform - 네이트온 이용가이드'
+			// (Optional) 아래는 필요에 따라 추가하며 상세한 내용은 하단 표를 참고하세요.
+			// sso_token: 'eyJ1c2VyX2lkIjoiZGhrYW5nIiwibmFtZSI6InVzZXJfbmFtZSIsInBob3RvIjoiaHR0cDovL3N0YXRpYzIubWUyZGF5LmNvbS9pbWFnZXMvdXNlci9nZWVraW5zaWRlL3Byb2ZpbGUucG5nPzEyNjY5MTExMzcifQ== 20c87b006bb6c433a6171e51c8534c8a1ea35d3f1a10681289193cd4ed463fc2973575bc1713169b0dc7593d9114a1d09128269c980d8d8251adf67b4a89240e 1338340593597',
+			lang: 'ko',
+			is_mobile: false,
+			theme: 'default',
+			// is_responsive: false
+	});
+</script>
