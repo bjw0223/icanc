043c7354eacd9d0a0fc92690998ce87f94b4fc11
icanc <bjw0223@naver.com>
2013-10-06 17:46:43 +0900

    icanc-sever

--- user/1/freeCode/freeCode.c
+++ user/1/freeCode/freeCode.c
@@ -1,68 +1,9 @@
 #include <pthread.h> 
 #include <unistd.h>
 #include <stdlib.h>
-<<<<<<< HEAD
-#include <stdio.h>
-void *runCode34567(void *id34567){	int i,j;
-	int n;
-	
-	char seekname[10]="hyunjung";
-	printf("찾고자 하는 이름 : %s\n", seekname);
-	char name[10][10]={"elle", 
-                       "yeomyeon", 
-                       "almang", 
-                       "hyunjung", 
-                       "han", 
-                       "heejung", 
-                       "mingjaen", 
-                       "killer", 
-                       "coke", 
-                       "solas"};
-	
-	for(i=0; i<10; i++)
-    {
-      	j=0;
-    	while(name[i][j]!='\0')
-        {
-        	j++;
-        }
-      	printf("%d번 글자수 : %d\n",i+1,j);
-      	n=j;
-      
-      	if(n==8)
-        {
-          	int flag=0;
-          
-    		for(j=0; j<n;j++)
-        	{
-         	 	if(name[i][j]!=seekname[j])
-          		{
-            		//printf("불일치\n");
-          			flag=1;
-                  	break;
-          		}
-            }
-          	if(flag==0)
-            {
-          		printf("일치\n");
-            }
-        }
-      	else
-        {
-          //printf("불일치\n");
-        }
-    }}
-
-void *checkTime34567(void *id34567)
-=======
 #include <stdio.h>void *runCode34567(void *id34567) 
-<<<<<<< HEAD
-{	return 0;}void *checkTime34567(void *id34567)
->>>>>>> 1bcb0514edbd65a7545a5cbc91bb6f9b7cc89623
-=======
-{	printf("dd");    return 0;
+{printf("ddd");    return 0;
 }void *checkTime34567(void *id34567)
->>>>>>> 84bd042e42ef9f3fb4a78a4bd7a2d8508f122c23
 {
     sleep(2);
     
